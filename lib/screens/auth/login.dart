88917ffdb2b72874913b04daf4390cba71dc7068
ivan sempijja <ivansempijja@gmail.com>
2022-12-09 15:23:55 +0300

    add icon lancher and update login page logo

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -25,118 +25,119 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: ArgonColors.white,
         body: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: const <Widget>[
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: const <Widget>[
+                    SizedBox(
+                      child: Image(
+                        image: AssetImage('assets/img/logo2.png'),
+                        height: 90,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                          fontSize: 22.0, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.left,
+                    )),
+                const SizedBox(
+                  height: 15.0,
+                ),
                 SizedBox(
-                  child: Image(
-                    image: AssetImage('assets/img/logo.png'),
-                    height: 70,
-                    width: 70,
+                    child: BootstrapAlert(
+                  visible: authPassed,
+                  text: errorMessage,
+                  status: AlertStatus.danger,
+                )),
+                SizedBox(
+                  width: double.infinity,
+                  child: Input(
+                    placeholder: "Email Address",
+                    prefixIcon: const Icon(Icons.email),
+                    controller: emailController,
+                    validator: Helpers.validateEmail,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Input(
+                    placeholder: "Password",
+                    isPassword: true,
+                    prefixIcon: const Icon(Icons.lock),
+                    controller: passwordController,
+                    validator: Helpers.validatePassword,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      loginFunction();
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: ArgonColors.primary,
+                        padding: const EdgeInsets.only(top: 15, bottom: 15)),
+                    child: isLoading
+                        ? const SizedBox(
+                            height: 15,
+                            width: 15,
+                            child: CircularProgressIndicator(
+                              color: ArgonColors.white,
+                              strokeWidth: 1,
+                            ))
+                        : const Text(
+                            "Login",
+                            style: TextStyle(color: ArgonColors.white),
+                          ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/register");
+                        },
+                        child: const Text(
+                          'Create Account',
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      const Text(
+                        'Reset Password',
+                        textAlign: TextAlign.right,
+                      )
+                    ],
                   ),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const SizedBox(
-                width: double.infinity,
-                child: Text(
-                  "Sign In",
-                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.left,
-                )),
-            const SizedBox(
-              height: 15.0,
-            ),
-            SizedBox(
-                child: BootstrapAlert(
-              visible: authPassed,
-              text: errorMessage,
-              status: AlertStatus.danger,
-            )),
-            SizedBox(
-              width: double.infinity,
-              child: Input(
-                placeholder: "Email Address",
-                prefixIcon: const Icon(Icons.email),
-                controller: emailController,
-                validator: Helpers.validateEmail,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: Input(
-                placeholder: "Password",
-                isPassword: true,
-                prefixIcon: const Icon(Icons.lock),
-                controller: passwordController,
-                validator: Helpers.validatePassword,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  loginFunction();
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: ArgonColors.primary,
-                    padding: const EdgeInsets.only(top: 15, bottom: 15)),
-                child: isLoading
-                    ? const SizedBox(
-                        height: 15,
-                        width: 15,
-                        child: CircularProgressIndicator(
-                          color: ArgonColors.white,
-                          strokeWidth: 1,
-                        ))
-                    : const Text(
-                        "Login",
-                        style: TextStyle(color: ArgonColors.white),
-                      ),
-              ),
-            ),
-            const SizedBox(height: 15),
-            SizedBox(
-              width: double.infinity,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/register");
-                    },
-                    child: const Text(
-                      'Create Account',
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  const Text(
-                    'Reset Password',
-                    textAlign: TextAlign.right,
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 
   loginFunction() async {
